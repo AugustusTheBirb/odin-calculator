a554a1cd6f7274fd6273f6a54db71c48bc61cc5e
AugustusTheBirb <augustasrink@gmail.com>
2025-02-05 02:21:32 +0200

    added calculator logic

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function mul(a,b) {
 
 function div(a,b) {
     if (b == 0) {return "DIVISION BY ZERO"}
-    return parseFloat(a) / parseFloat(b)
+    return Math.round(1000*parseFloat(a) / parseFloat(b))/1000
 }
 
 function percent(a) {
@@ -23,15 +23,87 @@ function sign(a) {
     return -1 * parseFloat(a)
 }
 
-let num1 = 0
-let num2 = 0
-let op = ''
+function update(){
+    if (op === '') {
+        display.innerText = num1
+    }
+    else {
+        display.innerText = num2
+    }
+}
 
-const numberButtons = document.querySelectorAll(".nm")
-const operationButtons = document.querySelectorAll(".operation")
+function chooseOperation(val){
+    switch (val){
+        case "%":
+            num1 = String(percent(num1))
+            update()
+            break
+        case "+/-":
+            num1 = String(sign(num1))
+            update()
+            break
+        default:
+            op = val;
+    }
+    
+}
 
-const clearButton = document.querySelector(".clear")
+function updateNum(val){
+    if (op === '') { //means this is the first number, since no op
+        num1 += val
+    }
+    else {
+        num2 += val
+    }
+    update()
+}
+
+function calculate(){
+    switch (op) {
+        case "+":
+            num1 = String(add(num1, num2))
+            break
+        case "-":
+            num1 = String(sub(num1, num2))
+            break
+        case "*":
+            num1 = String(mul(num1, num2))
+            break
+        case "/":
+            num1 = String(div(num1, num2))
+            break
+    }
+    op = ''
+    num2 = ''
+    update()
+}
+
+let num1 = '';
+let num2 = '';
+let op = '';
+
+const display = document.querySelector(".text")
+
+const numberButtons = document.querySelectorAll(".nm");
+const operationButtons = document.querySelectorAll(".operation");
+
+const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals")
+
+clearButton.addEventListener("click", () => {
+    num1 = ''
+    op = ''
+    num2 = ''
+    update()
+})
+
+equalsButton.addEventListener("click", calculate)
+
+numberButtons.forEach(element => {
+    element.addEventListener("click", () => updateNum(element.value))    
+});
 
 operationButtons.forEach(element => {
-    element.addEventListener("click", )    
-});
+    element.addEventListener("click", () => chooseOperation(element.value))
+})
+
